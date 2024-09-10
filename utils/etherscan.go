ce78b749546fd988f5f25b59d414b5ee489dcf98
Marsot Pierre <pierre.mmarsot@gmail.com>
2024-09-10 14:21:18 +0200

    fix pending fees

--- utils/etherscan.go
+++ utils/etherscan.go
@@ -6,17 +6,24 @@ import (
 	"strings"
 )
 
-func ContainsWithdrawAdminFees(contractAddress string) (bool, error) {
-
+func GetEtherscanAbi(contractAddress string) (string, error) {
 	response, err := http.Get("https://api.etherscan.io/api?module=contract&action=getabi&address=" + contractAddress + "&apikey=" + GoDotEnvVariable("ETHERSCAN_API_KEY"))
 	if err != nil {
-		return false, err
+		return "", err
 	}
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return false, err
+		return "", err
 	}
 
-	return strings.Contains(string(body), "withdraw_admin_fees"), nil
+	return string(body), nil
+}
+
+func ContainsWithdrawAdminFees(abi string) (bool, error) {
+	return strings.Contains(abi, "withdraw_admin_fees"), nil
+}
+
+func ContainsClaimAdminFees(abi string) (bool, error) {
+	return strings.Contains(abi, "claim_admin_fees"), nil
 }
